aa0703a4c6206c70956caf0581262293150f1c22
Amy Zhou <aymeighz@gmail.com>
2018-08-10 18:55:02 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,9 @@
 def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    return  "I LOVE YOU TOO PUMPKIN!"
   if phrase == phrase.upcase
-  return "I LOVE YOU TOO PUMPKIN!"
-else
-  return "HUH?! SPEAK UP, SONNY!"
+    return "NO, NOT SINCE 1938!"
+  else
+   return "HUH?! SPEAK UP, SONNY!"
 end
 end
